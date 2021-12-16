ae4a9b7e131116d9c1df41bdb06f6e81102b52f9
Quinn Barnett <squinnmb@gmail.com>
2021-12-16 23:02:26 +0900

    Refactor Reviews component to keep code DRY, make RevieweForm Component reusable

--- src/components/ReviewComponents/ReviewForm.tsx
+++ src/components/ReviewComponents/ReviewForm.tsx
@@ -3,54 +3,32 @@ import {AuthContext} from '../contexts/AuthContext'
 import '../../details.css'
 
 interface Props {
-    book_id: number;
-    setHasReviewed: Function;
+    reviewTitle: string;
+    setReviewTitle: Function;
+    reviewText: string;
+    setReviewText: Function;
+    // This will be the POST, PUT, or DELETE review function
+    sendRequest: Function;
 }
 
 export default function ReviewForm(props: Props) {
-    const baseUrl = process.env.REACT_APP_BASE_URL
-    const {book_id, setHasReviewed} = props
-    let {authTokens, user} = useContext(AuthContext)
-    
-    
-    let [reviewTitle, setReviewTitle] = useState("")
-    let [reviewText, setReviewText] = useState("")
-    
+    const {sendRequest, setReviewText, setReviewTitle, reviewText, reviewTitle} = props
+
     const handleChange = (e: any) => {
         if(e.target.name === "reviewTitle") { setReviewTitle(e.target.value) }
         else if(e.target.name === "reviewText") { setReviewText(e.target.value) }
     }
 
-    const sendReview = async (e: any) => {
+    const handleSubmit = (e: any) => {
         e.preventDefault()
-        const context = {
-            title: reviewTitle,
-            text: reviewText,
-            user: user.user_id,
-            book: book_id
-        }
-        if(reviewText.trim().length === 0 || reviewTitle.trim().length === 0) { return }
-        let response = await fetch(`${baseUrl}review/${book_id}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authTokens.access}`
-            },
-            body: JSON.stringify(context) }
-        )
-        let data = await response.json()
-        console.log("Data: ", data)
-        console.log("Response: ", response)
-        if(response.status == 201) {
-            setHasReviewed(true)
-        }
+        sendRequest()
     }
 
     return (
-        <form action="submit" onSubmit={(e) => sendReview(e)} className='review_form'>
-            <input className='review_form_title' type="text" name='reviewTitle' onChange={(e) => handleChange(e)} placeholder='Review Title' />
-            <textarea className='review_form_text' name="reviewText"  onChange={(e) => handleChange(e)}></textarea>
-            <button className='review_form_btn'>Send Review</button>
+        <form action="submit" onSubmit={(e) => handleSubmit(e)} className='review_form'>
+            <input className='review_form_title' value={reviewTitle} type="text" name='reviewTitle' onChange={(e) => handleChange(e)} placeholder='Review Title' />
+            <textarea className='review_form_text' value={reviewText} name="reviewText"  onChange={(e) => handleChange(e)}></textarea>
+            <button className='review_form_btn'>レビュー送信</button>
         </form>
     )
 }
--- src/components/ReviewComponents/Reviews.tsx
+++ src/components/ReviewComponents/Reviews.tsx
@@ -15,12 +15,16 @@ interface ReviewData {
     title: string;
 }
 export default function Reviews(props: Props) {
-    let {user} = useContext(AuthContext)
+    let {user, authTokens} = useContext(AuthContext)
     const { book_id } = props
     const baseUrl = String(process.env.REACT_APP_BASE_URL)
 
     let [hasReviewed, setHasReviewed] = useState<Boolean>(true)
-    let [reviews, setReviews] = useState<ReviewData[] | null>(null)
+    let [reviews, setReviews] = useState<ReviewData[]>([])
+
+    // The user's review
+    let [reviewTitle, setReviewTitle] = useState("")
+    let [reviewText, setReviewText] = useState("")
 
     const getReviews = async () => {
         try {
@@ -28,7 +32,8 @@ export default function Reviews(props: Props) {
             if(response.status === 200) {
                 let data = await response.json()
                 console.log("Reviews I got them: ", data)
-                if(user) { 
+                if(user) {
+                    // must change this later, poor time complexity if many reviews 
                     for(let i = 0; i < data.length; i++) {
                         if(data[i].user === user.name) {
                             console.log(data[i].user) 
@@ -49,6 +54,34 @@ export default function Reviews(props: Props) {
         } catch(e) { console.log("Error fetching reviews", e) }
     }
 
+    const sendPost = async (e: any) => {
+        const context = {
+            title: reviewTitle,
+            text: reviewText,
+            user: user.user_id,
+            book: book_id
+        }
+        if(reviewText.trim().length === 0 || reviewTitle.trim().length === 0) { return }
+        let response = await fetch(`${baseUrl}review/${book_id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authTokens.access}`
+            },
+            body: JSON.stringify(context) }
+        )
+        let data = await response.json()
+        console.log("Data: ", data)
+        console.log("Response: ", response)
+        if(response.status == 201) {
+            setHasReviewed(true)
+            let newReview: ReviewData = data
+            setReviews((prevReviews) => {
+                return [newReview , ...prevReviews]
+            })
+        }
+    }
+
     useEffect(() => {
         getReviews()
     },[])
@@ -57,7 +90,7 @@ export default function Reviews(props: Props) {
         <div className='review'>
             {
                 user && !hasReviewed ?
-                <ReviewForm setHasReviewed={setHasReviewed}  book_id={book_id}/> :
+                <ReviewForm sendRequest={sendPost} setReviewText={setReviewText} reviewText={reviewText} setReviewTitle={setReviewTitle} reviewTitle={reviewTitle} /> :
                 ""
 
             }
